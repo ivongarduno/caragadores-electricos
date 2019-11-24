39e5584f6361289f99b5d45f8a8aa9613643277f
BLANK-TECH <ivonne_1412@hotmail.com>
2019-11-24 15:13:45 -0600

    Updates

---
+++ precache-manifest.f8abc0a74b680cbc548d587ed5b31cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09915e026e1b0de7c69a4ff5b5e1c26d",
+    "url": "/caragadores-electricos/index.html"
+  },
+  {
+    "revision": "11aa072f00a03dc5ff3f",
+    "url": "/caragadores-electricos/static/css/2.8b70035b.chunk.css"
+  },
+  {
+    "revision": "ab170267230b12d82252",
+    "url": "/caragadores-electricos/static/css/main.463d74f4.chunk.css"
+  },
+  {
+    "revision": "11aa072f00a03dc5ff3f",
+    "url": "/caragadores-electricos/static/js/2.2d44d41c.chunk.js"
+  },
+  {
+    "revision": "ab170267230b12d82252",
+    "url": "/caragadores-electricos/static/js/main.6d05a81d.chunk.js"
+  },
+  {
+    "revision": "5a14523ccfc0d45ead7a",
+    "url": "/caragadores-electricos/static/js/runtime-main.02d67195.js"
+  }
+]);
